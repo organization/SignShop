757624496b96b73e0bb2847022d7d9b1522cb483
ZINGDING <tjgmltn07@naver.com>
2016-11-05 19:06:32 +0900

    Release v0.0.2-SNAPSHOT.jar Fix detailed error.

---
+++ src/main/java/list.java
@@ -0,0 +1,12 @@
+//TODO LIST
+/* 1. Add Item in glass Block. => like EconomySell ; Shop 
+ * 2. Add Lapis Lazuli Ore (Mineral) at Price.txt
+*/
+
+//Update Note
+/* 1. Release v0.0.1-SnapShot. - Permission Error & Path Error
+ * 2. DeBug.
+ * 3. Release v0.0.2-SnapShot. - Detailed Error & There is no Lapis Lazuli Ore (Mineral) at price.txt
+ * 4. DeBug.
+ * 5. Add v0.0.2-SnapShot at Release Tab. - There is no Lapis Lazuli Ore (Mineral) at price.txt
+ */
